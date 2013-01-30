53fee603cbc0c082e564d35246cf77bb54e0657f
Lance Vo <lvo@vinavo.us>
2013-01-30 16:51:40 -0600

    refactor smartform.js

--- js/smartform.js
+++ js/smartform.js
@@ -1,136 +1,117 @@
-/* smartform 
-
-  a form plug-in to validate inputs and very flexible to customize notifications.
-
-  it adds necessary classes to input container so you can customize your message to however you see fit. 
-  
-  ** note ** : if the value is empty, no pattern test is applied meaning there's no class "valid" or "invalid" in parent container. 
-
-  
-  
-
-  to do: 
-    - test for textarea / select
-    - clone base notifications
-    - allow override notifcations
-    - select
-    - form submit validation
-
-  
-*/
-
 /* 
   smartform
   A plug-in to validate form fields. It utilizes HTML5 form field attributes. 
   It adds necessasy classes to field container as, and so you can customize your messages 
-  https://github.com/lancevo/smartform
+  
+  source: https://github.com/lancevo/smartform
+  // requires jQuery 1.7+
 */
 
-// requires jQuery 1.7+
 
 // a helper to add/remove classes to input container
-$.fn.yayNay = function(condition, classesToAdd, classesToRemove) {
-  "use strict";
-  var $el = $(this),
-      classesToRemove = classesToRemove || classesToAdd;
+// @param removeClasses is optional, if undefined it'll be the same as addClasses
+$.fn.yayNay = function(condition, addClasses, removeClasses) {
+  var el = $(this),
+      removeClasses = typeof removeClasses === 'undefined' ? addClasses : removeClasses
+      ;
 
   condition ? 
-    $el.addClass(classesToAdd) :
-    $el.removeClass(classesToRemove)
+    el.addClass(addClasses) :
+    el.removeClass(removeClasses)
+
+  return this;
 }
 
 $.fn.smartform = function() {
-
   // customize classes for input field container
-  var stateClasses = {
-    	'mouseover'     : 'hover',
-    	'mouseout'      : 'hover',
-    	'focus'         : 'focus',
-    	'focusin'       : 'focus',
-    	'focusout'      : 'visited',
-    	'blur'          : 'visited',
-    	'invalid'       : 'invalid',
-    	'valid'         : 'valid',
-    	'required'			: 'required',
-    	'unchecked'     : 'unchecked',
-    	'checked'       : 'checked',
-    	'indeterminate' : 'indeterminate',
-    	'selected'      : 'selected',
-    	'unselected'    : 'unselected',
-    	'in-range'      : 'in-range',
-    	'out-of-range'  : 'out-of-range'
-    },
-    $form = $(this);
+  var $form = $(this),
+      // html5 browser validator
+      checkValidity = typeof document.createElement( 'input' ).checkValidity === 'function';
 
-
+  $form.attr('novalidate','novalidate')
 
   function validateField(ev){
-    var $el = $(this),
-        $wrapper = $el.attr('data-smartform-container') ? $el.closest( $el.attr('data-smartform-container') ) : $el.parent(),
-        $messages = $el.find( $el.attr('data-smartform') ),
-        val = $el.val().replace(/^\s+$/g,''), // remove empty spaces 
-        pattern = $el.attr('pattern') ? new RegExp( $el.attr('pattern') ) : false,
-        isValid =  true,
-        isRequired = $el.attr('required'),
-        isCheckbox = $el.attr('type') == 'checkbox',
-        isRadio = $el.attr('type') == 'radio',
-        isSelect = $el.is('select'),
-        isTextarea = $el.is('textarea'),
-        elName = $el.attr('name')     
+    ev.stopPropagation();
+    var el = $(this),
+        elContainer = el.attr('data-smartform') ? el.closest( el.attr('data-smartform') ) : el.parent(),
+        value = el.val().replace(/^\s+$/g,''), // remove empty spaces 
+        pattern = el.attr('pattern') ? new RegExp( el.attr('pattern') ) : undefined,
+        isRequired = el.attr('required'),
+
+        isCheckbox = el.attr('type') === 'checkbox',
+        isRadio = el.attr('type') === 'radio',
+        isSelect = el.is('select'),
+        isTextarea = el.is('textarea'),
+
+        isValid = pattern ? pattern.test(value) : true 
         ;
 
-    ev.stopPropagation();
+    // visited
+    // add .visited if it doesn't have it
+    elContainer.yayNay( !elContainer.hasClass('visited'), 'visited', '');
 
-    // test pattern for non-empty value
-    if (val!== '' && pattern) {
-      isValid = pattern.test( val )
-    }
-
-    // add any valid type to the wrapper by default
-    stateClasses[ev.type] ? $wrapper.addClass( stateClasses[ev.type] ) : '';
-
+    // focus
     switch (ev.type) {
-      //case 'focusin' : 
-      //          $wrapper.removeClass( stateClasses['hover'] )
-      //          break;
-
+      case 'focusin' :
+            elContainer.addClass('focus')
+            break
+      
       case 'focusout': 
-      case 'blur'    :
       case 'change'  :
-                $wrapper.removeClass( stateClasses['focusin'] ); 
-                
-                // validate the pattern when value is not empty and there is a pattern
-                $wrapper.yayNay( (val!=='' && !isValid), stateClasses['invalid'] );
+      case 'blur'    :
+      case 'select'  : 
+            // required
+            elContainer.removeClass('focus')
 
-                // add class "valid" or "invalid" when the value and pattern are not empty 
-                $wrapper.yayNay( (val!=='' && isValid && pattern), stateClasses['valid'] );
+            if (isCheckbox || isRadio) {
+              var isChecked = el.is(':checked');
+                                  
+              if (isRadio) {
+                isChecked = $form.find('input[name="' + el.attr('name') + '"]').is(':checked');
+                $form.find('input[name="' + el.attr('name') + '"]').each(function(){
+                  if ($(this).attr('required')) {
+                    isRequired = true;
+                  }
+                })
+              }
 
-                // add class "visited", there's no focusout/blur for checkbox 
-                $wrapper.yayNay( isCheckbox , stateClasses['blur'] , ' ')
-                $wrapper.yayNay( isCheckbox && $el.is(':checked'), stateClasses['checked'])
-                $wrapper.yayNay( isCheckbox && !$el.is(':checked'), stateClasses['unchecked'])
-
-                //  careful here, don't let $wrapper.yayNay overrides each other add/remove class
-                if (isCheckbox || isRadio) {
-                  $wrapper.yayNay( isCheckbox && isRequired && !$el.is(":checked"), stateClasses['required'])
-                } else {
-                  $wrapper.yayNay(isRequired && val ==='', stateClasses['required']);
-                }
-
-                // radio
-                $wrapper.yayNay( isRadio , stateClasses['blur'] + ' checked', ' ')
-
-                break;
-
-      case 'mouseout' : 
-                $wrapper.removeClass(stateClasses['mouseout']);
-                break;
+              elContainer.yayNay( isChecked, 'checked' )
+                         .yayNay( !isChecked, 'unchecked' )
+                         .yayNay( isRequired && !isChecked, 'required')
+            } else {
+              elContainer.yayNay( isRequired && value==='', 'required')
+                         .yayNay( pattern && isValid, 'valid' )
+                         .yayNay( pattern && !isValid, 'invalid' )
+            }
+            break
     }
-    
+ 
   } // validateField
 
-  $form.on("focus blur change keyup mouseover mouseout",":input", validateField)
 
+  $form.on("blur change focus focusin focusout select",":input", validateField)
+
+  $form.submit( function(ev){
+    var isValidated = true;
+    
+    $form.find(":input").each(function(){
+      var el = $(this),
+          elContainer = el.attr('data-smartform') ? el.closest( el.attr('data-smartform') ) : el.parent();
+
+      el.blur()
+      
+      if (elContainer.hasClass('invalid') || elContainer.hasClass('required')) {
+        isValidated = false;
+      }
+    })
+
+    if (!isValidated) {
+      $form.addClass('submit-invalid')
+      return false
+    } else {
+      $form.addClass('submit-valid')
+    }
+  }) // submit
 
 }; // smartform
 
